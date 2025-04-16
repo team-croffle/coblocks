4b1088e4914a3e503a6480991e843311a886ecc8
unbaen <rlagotjd62@gmail.com>
2025-04-16 20:13:20 +0900

    fix: Language hide,Add icon,Add imgage
    -언어변경 기능 숨김처리
    -아이콘 추가
    -디스플레이 크기에 맞추어 자동적으로 페이지 변경
    -공통 이미지 추가
    
    Ref:CBLS-118,CBLS-119

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>User</h1>
+      <p>This is a dummy User page.</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
