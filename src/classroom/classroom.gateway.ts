6dda71267bad199076a5657eed8454ad6458a028
sejung2 <twindrop@naver.com>
2025-07-13 22:32:13 +0900

    feat: classroomgateway - 강의실 생성 메서드 구현 handleCreateRoom 메서드 구현 data는 현재 any로 받지만 후에 DTO로 교체 예정

--- src/classroom/classroom.gateway.ts
+++ src/classroom/classroom.gateway.ts
@@ -1,6 +1,7 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { ClassroomService } from './classroom.service';
 
 @WebSocketGateway({
   cors: {
@@ -8,6 +9,8 @@ import { OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
   },
 })
 export class ClassroomGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly classroomService: ClassroomService) {}
+
   // 서버로부터 클라이언트로 이벤트를 전송할 수 있는 서버 인스턴스를 가져옴
   @WebSocketServer()
   server: Server
@@ -22,5 +25,17 @@ export class ClassroomGateway implements OnGatewayConnection, OnGatewayDisconnec
     console.log(`Client disconnected: ${client.id}`);
   }
 
-  
+  // 소켓 연결에 성공한 사용자가 방을 개설할 때 호출되는 메서드
+  @SubscribeMessage('createRoom')
+  handleCreateRoom(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
+    const { id, code, managerId } = data; // 클라이언트로부터 받은 데이터
+    const managerSocketId = client.id; // 개설자 소켓 ID
+    try {
+      const newRoom = this.classroomService.createRoom(id, code, managerId, managerSocketId);
+      client.join(code); // 방에 참가
+      client.emit('roomCreated', newRoom); // 방 생성 성공 응답
+    } catch (error) {
+      client.emit('error', { message: error.message }); // 에러 발생 시 클라이언트에 에러 메시지 전송
+    }
+  }
 }
