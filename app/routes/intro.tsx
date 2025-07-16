e57a01083fb40da2be097bb87763cef0b6f1ddab
DESKTOP-4A3E8VG\김지훈 <kmr2397@naver.com>
2025-07-16 21:29:51 +0900

    refactor: 메인 페이지 레이아웃

---
+++ app/routes/intro.tsx
@@ -0,0 +1,28 @@
+//app/routes/intro
+
+import { Outlet } from '@remix-run/react';
+
+export default function IntroLayout(): JSX.Element {
+  return (
+    <div className='relative h-[1000px] bg-[linear-gradient(-5deg,rgb(202,244,255)_10%,rgb(160,222,255)_50%,rgb(90,178,255)_100%)]'>
+      <div className='relative z-20 mx-auto max-w-4xl px-4 py-12'>
+        <Outlet />
+      </div>
+
+      <div className='absolute bottom-0 left-0 hidden w-full overflow-hidden leading-none lg:block'>
+        <svg
+          className='relative block h-[230px] w-[calc(130%+1.3px)]'
+          data-name='Layer 1'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 1200 120'
+          preserveAspectRatio='none'
+        >
+          <path
+            d='M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z'
+            className='fill-white'
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
