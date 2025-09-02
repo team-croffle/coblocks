3f2190fd834beb8e10daefddea5546c0ba7ac800
BlueNyang <xpsj20@gmail.com>
2025-09-03 02:11:19 +0900

    fix: socket for blockly workspace and editor

--- app/components/SocketProvider.tsx
+++ app/components/SocketProvider.tsx
@@ -1,7 +1,8 @@
+import { useNavigate } from '@remix-run/react';
 import { useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { SocketContext } from '~/context/socket-context';
-import { SupabaseAuthToken } from '~/types/supabase';
+import { supabase } from '~/utils/supabase.client';
 
 interface SocketProviderProps {
   children: React.ReactNode;
@@ -9,20 +10,25 @@ interface SocketProviderProps {
 
 export function SocketProvider({ children }: SocketProviderProps) {
   const [socket, setSocket] = useState<Socket | null>(null);
-  const [currentToken, setCurrentToken] = useState<SupabaseAuthToken | null>(null);
+  const [currentToken, setCurrentToken] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     try {
-      const tokenStr = localStorage.getItem('supabase');
+      const checkSession = async () => {
+        const {
+          data: { session },
+        } = await supabase.auth.getSession();
+        setCurrentToken(session?.access_token || null);
+      };
 
-      if (!tokenStr) {
+      checkSession();
+
+      if (!currentToken) {
         console.log('[ClassroomPage] authenticateUser: 토큰이 없습니다.');
         alert('로그인이 필요합니다.');
         // main 으로 navigate
-        window.location.href = '/';
-      } else {
-        const token: SupabaseAuthToken = JSON.parse(tokenStr);
-        setCurrentToken(token);
+        navigate('/login');
       }
     } catch (error) {
       return;
