89318a35d7a42eedbe79169f5f773d3a303390cb
BlueNyang <kai@bluenyang.kr>
2026-09-04 22:07:12 +0900

    mono-repo

--- backend/src/modules/activity/activityDto/SubmitSolution.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class SubmitSolutionDto {
-  @IsString()
-  submissionContent!: string; // Blockly 데이터
-}
--- backend/src/modules/classroom/classroom.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClassroomGateway } from './classroom.gateway';
-import { ClassroomService } from './classroom.service';
-
-@Module({
-  providers: [ClassroomGateway, ClassroomService],
-  exports: [ClassroomService],
-})
-export class ClassroomModule {}
