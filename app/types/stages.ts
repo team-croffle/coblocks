a0bb1783be5383e93e7b644c39312e490b5fb55d
BlueNyang <kai@bluenyang.kr>
2026-09-04 21:29:07 +0900

    delete: legacy jsx

--- app/types/stages.ts
+++
@@ -1,9 +0,0 @@
-import { CodeSet, EntityDefinition, StageData } from '@croffledev/coblocks-stage-core';
-import { StageConfig } from '@croffledev/coblocks-stage-react';
-
-export type StageDataSet = {
-  config: StageConfig;
-  entityDefinitions: EntityDefinition[];
-  stageData: StageData;
-  codes: CodeSet;
-};
